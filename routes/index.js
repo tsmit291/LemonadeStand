bd82fe0d4e6c7c23255c145b7ca513df6ac9fffc
= <tsmit291@icloud.com>
2016-03-29 14:03:12 -0600

    header in the works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
 });
+
+router.get('/home', function(req, res, next){
+
+})
 
 module.exports = router;
